f0af1032ac4151c45e09c7cf8514dfd319880a08
kennybsmooth <kennybui@Kennys-MacBook-Air.local>
2016-11-11 15:15:09 -0500

    Added a Reservations controller

--- app/controllers/reservation_controller.rb
+++ app/controllers/reservation_controller.rb
@@ -1,19 +1,52 @@
 class ReservationController < ApplicationController
-  def new
-  end
-
-  def create
+  def index
+    @reservations = Reservation.all
   end
 
   def show
+    @reservation = Reservation.find(params[:id])
+
+    if current_user
+      @review = @reservation.reviews.build
+    end
+  end
+
+  def new
+    @reservation = Reservation.new
   end
 
   def edit
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def create
+    @reservation = Reservation.new(product_params)
+
+    if @reservation.save
+      redirect_to restaurants_url
+    else
+      render :new
+    end
   end
 
   def update
+    @reservation = Reservation.find(params[:id])
+
+    if @reservation.update_attributes(product_params)
+      redirect_to restaurants_url(@reservation)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    redirect_to restaurants_url
+  end
+
+  private
+  def reservation_params
+    params.require(:product).permit(:name, :description, :price_in_cents)
   end
 end

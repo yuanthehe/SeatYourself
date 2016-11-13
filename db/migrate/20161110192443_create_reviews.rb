e321420eb400ecac57b01755c4feac983ff16d6f
kennybsmooth <kennybui@Kennys-MacBook-Air.local>
2016-11-13 15:24:57 -0500

    Fixed syntax error in CreateReviews class

--- db/migrate/20161110192443_create_reviews.rb
+++ db/migrate/20161110192443_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[5.0]
   def change
     create_table :reviews do |t|
       t.integer :user_id
-      t.integer :restaurant_idid
+      t.integer :restaurant_id
       t.text :content
       t.integer :rating
 

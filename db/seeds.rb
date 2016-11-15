997b661752967b6e9f8fbd02709374dcd2d4a484
Michael <mjames.ayres@gmail.com>
2016-11-14 20:56:22 -0500

    Created 3 boxes on user show page for displaying user info, reviews, and reservation, styled notices and alerts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-m1 = Restaurant.create([{name: 'Pizza Hut' }, {capacity: 30 }, {location: 'Yonge & Lawrence' }, {phone: '(416)789-5577' }, {food_type: 'Fast Food' }])
-m2 = Restaurant.create([{name: "Gino's" }, {capacity: 20 }, {location: 'Avenue & Eglinton'}, {phone: '(647)572-6472' }, {food_type: 'Italian'}])
-m3 = Restaurant.create([{name: 'Sacre Bleu' }, {capacity: 32 }, {location: 'Yorkville' }, {phone: '(416)443-5627' }, {food_type: 'French'}])
-r1 = Review.create([{user_id: 1}, {restaurant_id: 1}, {content: "Great restaurant!"}])
-u1 = User.create([{name: "Donald Trump"}, {email: "thedonald@email.com"}, {password: "test"}, {password_confirmation: "test"}])
+m1 = Restaurant.create([{name: 'Pizza Hut', capacity: 30, location: 'Yonge & Lawrence', phone: '(416)789-5577', food_type: 'Fast Food' }])
+m2 = Restaurant.create([{name: "Gino's", capacity: 20, location: 'Avenue & Eglinton', phone: '(647)572-6472', food_type: 'Italian'}])
+m3 = Restaurant.create([{name: 'Sacre Bleu', capacity: 32, location: 'Yorkville', phone: '(416)443-5627', food_type: 'French'}])
+r1 = Review.create([{user_id: 1, restaurant_id: 1, content: "Great restaurant!"}])
+u1 = User.create([{name: "Donald Trump", email: "thedonald@email.com", password: "test", password_confirmation: "test"}])
